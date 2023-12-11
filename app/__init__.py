a69cc05e109573cb607d6fc44fef18ec17d53e1f
Leonardo Gomes <leonardogs0919@gmail.com>
2023-12-11 00:14:08 -0300

    docs: :memo: Swagger
    Adicionando doc swagger

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,32 @@
 from flask import Flask
-from flask_openapi3 import Info, OpenAPI
+from flask_swagger_ui import get_swaggerui_blueprint
 
-from app.routes import (
-    auth_routes,
-    category_routes,
-    doc_router_bp,
-    finance_routes,
-    user_routes,
-)
+from app.routes import auth_routes, category_routes, finance_routes, user_routes
 
 from .config import app_config, default_config
 from .utils import jwt
 
-info = Info(title=".Finance API", description="A simple API to manage your finances", version="1.0.0")
-
 
 def create_app(config_name=None):
     app = Flask(__name__)
-    app = OpenAPI(__name__, info=info)
     jwt.init_app(app)
+
+    SWAGGER_URL = '/api/docs'
+    API_URL = '/static/swagger.json'
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': "Finance API"
+        }
+    )
 
     config_name = config_name or default_config
     app.config.from_object(app_config[config_name])
 
     # Register blueprints routes
+    app.register_blueprint(swaggerui_blueprint, uxrl_prefix=SWAGGER_URL)
     app.register_blueprint(auth_routes)
-    app.register_blueprint(doc_router_bp)
     app.register_blueprint(category_routes)
     app.register_blueprint(user_routes)
     app.register_blueprint(finance_routes)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,17 +1,7 @@
-from flask import Blueprint, redirect
-
 from .auth_routes import auth_routes_bp
 from .category_routes import category_routes_bp
 from .finance_routes import finance_routes_bp
 from .user_routes import user_routes_bp
-
-doc_router_bp = Blueprint('doc_router', __name__)
-
-
-@doc_router_bp.get('/')
-def docs():
-    return redirect('/openapi/swagger')
-
 
 category_routes = category_routes_bp
 user_routes = user_routes_bp
